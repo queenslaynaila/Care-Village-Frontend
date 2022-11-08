8ac5d3e3f64caa32bd626dbda6a6ce7afb84da65
nxoo <nxoo.dev@gmail.com>
2022-11-08 19:20:39 +0300

    removed href warnings

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -9,9 +9,9 @@ function Navbar() {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-light fs-5 mb-sm-5 mb-3 py-3">
+            <nav className="navbar navbar-expand-lg fs-5 mb-sm-5 mb-3 py-3">
                 <div className="container col-sm-9">
-                    <a className="navbar-brand" href="#" onClick={() => navigate("/")}>
+                    <a className="navbar-brand" href="/#" onClick={() => navigate("/")}>
                         <i className="bi bi-flower3"></i> Care Village
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -22,22 +22,22 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav fw-bold ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" href="#" onClick={() => navigate("/help")}>Help</a>
+                                <a className="nav-link" href="/#" onClick={() => navigate("/help")}>Help</a>
                             </li>
                             <li className="nav-item ms-sm-3 mb-sm-0 mb-2">
-                                <a className="nav-link login d-inline-flex" href="#"
+                                <a className="nav-link login" href="/#"
                                    onClick={() => navigate("/login")}>
                                     Login</a>
                             </li>
                             <li className="nav-item dropdown ms-sm-3">
-                                <a className="nav-link signup dropdown-toggle d-inline-flex"
-                                   href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a className="nav-link signup dropdown-toggle"
+                                   href="/#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Signup
                                 </a>
                                 <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item fw-bold" href="#"
+                                    <li><a className="dropdown-item fw-bold" href="/#"
                                            onClick={() => navigate("/client-signup")}>As a Client</a></li>
-                                    <li><a className="dropdown-item fw-bold" href="#"
+                                    <li><a className="dropdown-item fw-bold" href="/#"
                                            onClick={() => navigate("/sitter-signup")}>As a Sitter</a></li>
                                 </ul>
                             </li>
