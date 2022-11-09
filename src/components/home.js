ad9a79969f2c28432562559d8455d8bcc13f8ac1
nxoo <nxoo.dev@gmail.com>
2022-11-09 09:14:46 +0300

    css spacing

--- src/components/home.js
+++ src/components/home.js
@@ -7,22 +7,22 @@ import medical from '../assets/medical.svg'
 
 function Home() {
     return (
-        <div className="col-sm-9 py-5">
+        <div className="py-5">
             <div className="row mb-5">
-                <div className="col-sm-9 mb-sm-0 mb-5">
+                <div className="col-sm-8 mb-sm-0 mb-5">
                     <h1 className="fw-bold mb-4">Find a service carer near you</h1>
                     <p>Find the right sitter or nanny for your family</p>
                     <button className="btn btn-danger rounded-0 btn-lg home-get-started-btn">Get Started</button>
                     <button className="btn btn-outline-secondary rounded-0 btn-lg ms-2 ">I'm a Sitter</button>
                 </div>
-                <div className="col-sm-3">
+                <div className="col-sm-4">
                     <img src={grandma} height="250px" width="350px" alt="Grandma"/>
                 </div>
             </div>
             <h1 className="my-5">Categories</h1>
-            <div className="row mt-5">
-                <div className="col-auto p-1">
-                    <div className="card shadow p-2" style={{width: "12rem", alignItems: "center"}}>
+            <div className="row mt-5 justify-content-between">
+                <div className="col-sm-auto mb-2">
+                    <div className="card shadow p-2" style={{width: "15rem", alignItems: "center"}}>
                         <img src={baby} height="100px" width="100px" alt="Grandma"/>
                         <div className="card-body">
                             <h5 className="card-title">Baby Sitter</h5>
@@ -30,8 +30,8 @@ function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="col-auto p-1">
-                    <div className="card shadow p-2" style={{width: "12rem", alignItems: "center"}}>
+                <div className="col-auto mb-2">
+                    <div className="card shadow p-2" style={{width: "15rem", alignItems: "center"}}>
                         <img src={pet} height="100px" width="100px" alt="Grandma"/>
                         <div className="card-body">
                             <h5 className="card-title">Pet Sitter</h5>
@@ -40,8 +40,8 @@ function Home() {
                         </div>
                     </div>
                 </div>
-                <div className="col-auto p-1">
-                    <div className="card shadow p-2" style={{width: "12rem", alignItems: "center"}}>
+                <div className="col-auto mb-2">
+                    <div className="card shadow p-2" style={{width: "15rem", alignItems: "center"}}>
                         <img src={medical} height="100px" width="100px" alt="Grandma"/>
                         <div className="card-body">
                             <h5 className="card-title">Care Companion</h5>
@@ -49,8 +49,8 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <div className="col-auto p-1">
-                <div className="card shadow p-2" style={{width: "12rem", alignItems: "center"}}>
+            <div className="col-auto mb-2">
+                <div className="card shadow p-2" style={{width: "15rem", alignItems: "center"}}>
                     <img src={specialNeeds} height="100px" width="100px" alt="Grandma"/>
                     <div className="card-body">
                         <h5 className="card-title">Special Needs</h5>
