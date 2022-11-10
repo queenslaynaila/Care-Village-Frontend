e581a585c49388a723bf62493eeb9100e7bb34ca
soila.torome <soila.torome@student.moringaschool.com>
2022-11-11 01:31:39 +0300

    Fech data from the server

--- src/components/browsesitter.js
+++ src/components/browsesitter.js
@@ -1,31 +1,55 @@
 import React, { useEffect } from "react";
 import SittersCard from "./sitterscard";
-
-const sitterPageColumns = {
-    display: "flex",
-    justifyContent: "space-between",
-  };
+import '../styles/browsestyle.css'
+import { useState } from "react";
 
 
+function BrowseSitter() {
+    
 
-function BrowseSitter({sitters}) {
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4,
+          slidesToSlide: 3  
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 3,
+          slidesToSlide: 2  
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 
+        }
+      }
 
-    // const[sitters, setSitters] = useEffect([])
-    // useEffect(() => {
-    //     fetch("http://localhost:8000/sitters")
-    //     .then(res => res.json())
-    //     .then(setSitters)
-    // },[])
+      const[sitters, setSitters] = useState(null)
+    
+      useEffect(() => {
+          fetch("http://localhost:8000/joboffers")
+          .then(res => res.json())
+          .then(data => setSitters(data))
+      },[])
+    
+    
 
     return (
-        <div>
-            <div style={sitterPageColumns}>
-                {/* <div className="col">
-                    {sitters.map(sitter => <SittersCard sitter={sitter} />)}
-                </div> */}
-                <SittersCard/>
-            </div>
+        <div className="browse-page">
+            
+            {sitters?
+                sitters.map(sitter=><SittersCard sitter={sitter}></SittersCard>)
+            :<p>login</p>}
         </div>
+        // <div>
+        //     <div>
+        //         {/* <Carousel responsive={responsive}>
+        //            {sitters.map(sitter=><SittersCard sitter={sitter}></SittersCard>)}
+        //         </Carousel> */}
+        //         <SittersCard />
+        //     </div>
+        // </div>
         
     )
 }
