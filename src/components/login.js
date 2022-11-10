3eb6d381906739b912e05ff486a878cca610711c
nxoo <nxoo.dev@gmail.com>
2022-11-10 11:37:22 +0300

    fixed warnings'

--- src/components/login.js
+++ src/components/login.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useRef, useState, useEffect} from "react";
+import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 
 export default function Login() {
