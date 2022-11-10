f49c3a7f8953c1f7bf0447fed01d1b874109bffc
Queenslay Jema <queenslay.naila@student.moringaschool.com>
2022-11-10 13:44:50 +0300

    change 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -1,10 +1,14 @@
 import React from "react";
-
+import '../styles/404.css'
 function NotFound() {
+   const imageurl="https://cdn.sittercity.com/6a2f0c7513f5ee449cdad04bcebb6d48c9fa6243/assets/oops-cd666b7ea5ce5415801ad4f875223942ee77726280b1e6b223ebb711e1c39ba9.svg"
     return (
-        <div className="col-sm-9">
-            <h1 className="fw-bold">Page Not Found</h1>
-            <h1 className="fw-bold">404</h1>
+        <div className="wrapcontainer">
+            <img className="errorimg" src={imageurl}></img>
+            <h1 className="processerror">Something Went Wrong</h1>
+            <h2 className="soory">We are sorry we have encountered an error</h2>
+
+         <button className="processerrorbtn bg-danger">Contact Customer Success</button>
         </div>
 
     )
