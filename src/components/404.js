884e45014d222b82cf9906b2ead8e6aa357575e7
nxoo <nxoo.dev@gmail.com>
2022-11-10 18:44:28 +0300

    responsive 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -1,16 +1,14 @@
 import React from "react";
-import '../styles/404.css'
+import oops from '../assets/oops.svg'
+
 function NotFound() {
-   const imageurl="https://cdn.sittercity.com/6a2f0c7513f5ee449cdad04bcebb6d48c9fa6243/assets/oops-cd666b7ea5ce5415801ad4f875223942ee77726280b1e6b223ebb711e1c39ba9.svg"
     return (
-        <div className="wrapcontainer">
-            <img className="errorimg" src={imageurl}></img>
-            <h1 className="processerror">Something Went Wrong</h1>
-            <h2 className="soory">We are sorry we have encountered an error</h2>
-
-         <button className="processerrorbtn bg-danger">Contact Customer Success</button>
+        <div className="col-sm-9">
+            <img className="" src={oops}></img>
+            <h1 className="">Something Went Wrong</h1>
+            <p className="">Page not found</p>
+         <button className="btn btn-danger btn-lg rounded-0 fw-bold">Contact Support</button>
         </div>
-
     )
 }
 
