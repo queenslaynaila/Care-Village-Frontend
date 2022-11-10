ddef454b071f71fbbbd4001a012e609dc022c221
soila.torome <soila.torome@student.moringaschool.com>
2022-11-11 01:32:13 +0300

    fetch data from the server to the cards

--- src/components/sitterscard.js
+++ src/components/sitterscard.js
@@ -1,74 +1,51 @@
 import React from "react";
-//import sitterlogo from "../assets/sitter.png";
-//import { Link } from "react-router-dom";
+import '../styles/sitterscard.css'
+import { useNavigate } from "react-router-dom";
 
 function SittersCard({sitter}) {
-    
+
+    let navigate = useNavigate()
+    function handleClick(e) {
+        navigate("/clientsignup");
+    }
+
     return (
-    //   <div className="row row-cols-1 row-cols-md-2 g-4">
-    //         <div className="col">
-    //             <div className="card text-center shadow p-2">
-    //             <img src={sitterlogo} height="250px" width="350px" alt="..."></img>
-    //             <div className="card-body">
-    //                 <h5 className="card-title">Card title</h5>
-    //                 <ul class="list-group list-group-flush">
-    //                     <li class="list-group-item">An item</li>
-    //                     <li class="list-group-item">A second item</li>
-    //                     <li class="list-group-item">A third item</li>
-    //                 </ul>
-    //             </div>
-    //             </div>
+        <>
+        {sitter?
+        <div id="login-container" className="sitter-card">
+        <h1 id="sitter-username">
+            {sitter.name}
+        </h1>
+        <div className="sitter-description">
+            {sitter.gender}
+        </div>
+        <div className="sitter-description">
+            {sitter.keyskills}
+        </div>
+        <div className="sitter-description">
+            {sitter.about}
+        </div>
+        <div className="sitter-description">
+            {sitter.location}
+        </div>
+        <div className="button-div">
+         <button id="sitter-button" className="processerrorbtn bg-danger" onClick={() => navigate("/signup-as-client")}>Hire Me</button>
+        </div>
+        
+    </div>: <p>No sitter</p>
+    }
+        </>
+        
+    //     <div className="card shadow p-2" style={{width: "15rem", alignItems: "center"}}>
+    //         <img src="" height="100px" width="100px" alt="Grandma"/>
+    //         <div className="card-body">
+    //             <h5 className="card-title">{sitter.user_name}</h5>
+    //             <p className="card-text">{sitter.gender}</p>
+    //             <p className="card-text">{sitter.phone_number}</p>
+    //             <p className="card-text">{sitter.location}</p>
     //         </div>
-    //         <div className="col">
-    //             <div className="card text-center shadow p-2">
-    //             <img src={sitterlogo} height="250px" width="350px" alt="..."></img>
-    //             <div className="card-body">
-    //                 <h5 className="card-title">Card title</h5>
-    //                 <ul class="list-group list-group-flush">
-    //                     <li class="list-group-item">An item</li>
-    //                     <li class="list-group-item">A second item</li>
-    //                     <li class="list-group-item">A third item</li>
-    //                 </ul>
-    //             </div>
-    //             </div>
-    //         </div>
-    //         <div className="col">
-    //             <div className="card text-center shadow p-2">
-    //             <img src={sitterlogo} height="250px" width="350px" alt="..."></img>
-    //             <div className="card-body">
-    //                 <h5 className="card-title">Card title</h5>
-    //                 <ul class="list-group list-group-flush">
-    //                     <li class="list-group-item">An item</li>
-    //                     <li class="list-group-item">A second item</li>
-    //                     <li class="list-group-item">A third item</li>
-    //                 </ul>
-    //             </div>
-    //             </div>
-    //         </div>
-    //         <div className="col">
-    //             <div className="card text-center shadow p-2">
-    //             <img src={sitterlogo} height="250px" width="350px" alt="..."></img>
-    //             <div className="card-body">
-    //                 <h5 className="card-title">Card title</h5>
-    //                 <ul class="list-group list-group-flush">
-    //                     <li class="list-group-item">An item</li>
-    //                     <li class="list-group-item">A second item</li>
-    //                     <li class="list-group-item">A third item</li>
-    //                 </ul>
-    //             </div>
-    //             </div>
-    //         </div>
-    //     </div>
-        <div className="card shadow p-2" style={{width: "15rem", alignItems: "center"}}>
-            <img src="" height="100px" width="100px" alt="Grandma"/>
-            <div className="card-body">
-                <h5 className="card-title">{sitter.user_name}</h5>
-                <p className="card-text">{sitter.gender}</p>
-                <p className="card-text">{sitter.phone_number}</p>
-                <p className="card-text">{sitter.location}</p>
-            </div>
-        <button type="button" class="btn btn-danger">Explore</button>
-    </div>
+    //     <button type="button" class="btn btn-danger">Explore</button>
+    // </div>
     )
 };
 
